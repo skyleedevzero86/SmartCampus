12ae40aab78ba27fd4d2a89b0a1a2ae90b0f973f
이경용 <sleekydz86@icloud.com>
2025-11-30 01:38:50 +0900

    feat: 프로젝트 셋팅 backup
    - 채팅모듈,배치모듈,api모듈 Init

---
+++ backend/market-batch/src/main/java/com/sleekydz86/alarm/MarketBatchApplication.java
@@ -0,0 +1,13 @@
+package com.sleekydz86.alarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketBatchApplication.class, args);
+    }
+
+}
---
+++ backend/market-chat/src/main/java/com/sleekydz86/chat/MarketChatApplication.java
@@ -0,0 +1,13 @@
+package com.sleekydz86.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketChatApplication.class, args);
+    }
+
+}
