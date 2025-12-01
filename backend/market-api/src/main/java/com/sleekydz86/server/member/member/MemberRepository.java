4af72fe34f206457a12792886ede6019a91fbd75
이경용 <sleekydz86@icloud.com>
2025-12-01 23:41:39 +0900

    feat: Member 도메인 Repository 인터페이스 추가

---
+++ backend/market-api/src/main/java/com/sleekydz86/server/member/member/MemberRepository.java
@@ -0,0 +1,21 @@
+package com.sleekydz86.server.member.member;
+
+import com.sleekydz86.server.member.member.dto.ProductByMemberResponse;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Optional<Member> findById(Long id);
+
+    Optional<Member> findByNickname(String nickname);
+
+    Optional<Member> findByEmail(String email);
+
+    Member save(Member member);
+
+    boolean existsByEmail(String email);
+
+    List<ProductByMemberResponse> findProductsByMemberId(Long memberId);
+}
