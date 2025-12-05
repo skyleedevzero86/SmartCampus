2ea9e4a199fcb8df3f70ca0dfda1e03797839fbe
이경용 <sleekydz86@icloud.com>
2025-12-05 14:11:05 +0900

    feat: Community Board 도메인 Repository 및 Event 추가

---
+++ backend/market-api/src/main/java/com/sleekydz86/server/market/board/domain/BoardRepository.java
@@ -0,0 +1,23 @@
+package com.sleekydz86.server.market.board.domain;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+public interface BoardRepository {
+
+    Board save(Board board);
+
+    Optional<Board> findById(Long id);
+
+    Optional<BoardFoundResponse> findByIdForRead(Long boardId, Long memberId);
+
+    Page<BoardSimpleResponse> findAllBoardsWithPaging(Pageable pageable, Long memberId);
+
+    Optional<Board> findByIdUsingPessimistic(Long id);
+
+    Optional<Board> findBoardWithImages(Long boardId);
+
+    void deleteByBoardId(Long boardId);
+}
