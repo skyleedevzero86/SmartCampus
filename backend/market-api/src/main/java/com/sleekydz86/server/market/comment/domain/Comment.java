137d3ac8d1156c427a018402e3918c483203ba7e
이경용 <sleekydz86@icloud.com>
2025-12-05 14:16:59 +0900

    feat: Community Comment 도메인 추가

---
+++ backend/market-api/src/main/java/com/sleekydz86/server/market/comment/domain/Comment.java
@@ -0,0 +1,31 @@
+package com.sleekydz86.server.market.comment.domain;
+
+import com.sleekydz86.server.global.domain.BaseEntity;
+import lombok.*;
+
+@Getter
+@Builder
+@EqualsAndHashCode(of = "id", callSuper = false)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Comment extends BaseEntity {
+
+    private Long id;
+
+    private String content;
+
+    private Long writerId;
+
+    private Long boardId;
+
+    public void update(final String comment, final Long writerId) {
+        validateWriterId(writerId);
+        this.content = comment;
+    }
+
+    public void validateWriterId(final Long writerId) {
+        if (!this.writerId.equals(writerId)) {
+            throw new CommentWriterNotEqualsException();
+        }
+    }
+}
---
+++ backend/market-api/src/main/java/com/sleekydz86/server/market/comment/domain/CommentRepository.java
@@ -0,0 +1,20 @@
+package com.sleekydz86.server.market.comment.domain;
+
+
+import com.sleekydz86.server.market.comment.domain.dto.CommentSimpleResponse;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CommentRepository {
+
+    Comment save(Comment comment);
+
+    Optional<Comment> findById(Long id);
+
+    List<CommentSimpleResponse> findAllCommentsByBoardId(Long boardId, Long memberId, Long commentId, int pageSize);
+
+    void deleteById(Long id);
+
+    void deleteAllByBoardId(Long boardId);
+}
