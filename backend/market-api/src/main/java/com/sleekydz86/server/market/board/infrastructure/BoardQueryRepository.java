84835bab7b80c4337d5247c614b76e55babcb9a1
이경용 <sleekydz86@icloud.com>
2025-12-05 14:30:06 +0900

    feat: Community Infrastructure Board Repository 구현

---
+++ backend/market-api/src/main/java/com/sleekydz86/server/market/board/infrastructure/BoardQueryRepository.java
@@ -0,0 +1,79 @@
+package com.sleekydz86.server.market.board.infrastructure;
+
+import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+import com.querydsl.core.QueryResults;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.Optional;
+import static com.querydsl.core.types.ExpressionUtils.count;
+import static com.querydsl.core.types.Projections.constructor;
+
+@RequiredArgsConstructor
+@Repository
+public class BoardQueryRepository {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    public Page<BoardSimpleResponse> findAllBoard(final Pageable pageable, final Long memberId) {
+        QueryResults<BoardSimpleResponse> result = jpaQueryFactory.select(
+                        constructor(BoardSimpleResponse.class,
+                                board.id,
+                                member.nickname,
+                                board.post.title,
+                                board.createdAt,
+                                board.likeCount.likeCount,
+                                count(comment.id),
+                                isLikedAlreadyByMe(memberId)
+                        )).from(board)
+                .leftJoin(member).on(board.writerId.eq(member.id))
+                .leftJoin(comment).on(comment.boardId.eq(board.id))
+                .leftJoin(likeStorage).on(likeStorage.boardId.eq(board.id).and(likeStorage.memberId.eq(memberId)))
+                .groupBy(board.id, member.nickname, board.post.title, board.createdAt, board.likeCount.likeCount)
+                .orderBy(board.id.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetchResults();
+
+        return new PageImpl<>(result.getResults(), pageable, result.getTotal());
+    }
+
+    public Optional<BoardFoundResponse> findById(final Long boardId, final Long memberId) {
+        return Optional.ofNullable(
+                jpaQueryFactory.select(
+                                constructor(BoardFoundResponse.class,
+                                        board.id,
+                                        member.nickname,
+                                        board.post.title,
+                                        board.post.content,
+                                        board.likeCount.likeCount,
+                                        isMyPost(memberId),
+                                        isLikedAlreadyByMe(memberId),
+                                        board.createdAt
+                                )
+                        ).from(board)
+                        .leftJoin(member).on(board.writerId.eq(member.id))
+                        .leftJoin(likeStorage).on(likeStorage.boardId.eq(board.id).and(likeStorage.memberId.eq(memberId)))
+                        .where(board.id.eq(boardId))
+                        .fetchOne()
+        );
+    }
+
+    private BooleanExpression isLikedAlreadyByMe(final Long memberId) {
+        return new CaseBuilder()
+                .when(likeStorage.memberId.eq(memberId)).then(true)
+                .otherwise(false);
+    }
+
+    private BooleanExpression isMyPost(final Long memberId) {
+        return new CaseBuilder()
+                .when(board.writerId.eq(memberId)).then(true)
+                .otherwise(false);
+    }
+}
