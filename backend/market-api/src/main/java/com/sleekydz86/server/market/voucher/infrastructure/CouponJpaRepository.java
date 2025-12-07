62a1d2a0c40ab652027fe0950e67f5b4f3d1ec73
이경용 <sleekydz86@icloud.com>
2025-12-07 22:57:59 +0900

    feat: Coupon Infrastructure Coupon Repository 구현

---
+++ backend/market-api/src/main/java/com/sleekydz86/server/market/voucher/infrastructure/CouponJpaRepository.java
@@ -0,0 +1,20 @@
+package com.sleekydz86.server.market.voucher.infrastructure;
+
+import com.sleekydz86.server.market.coupon.domain.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CouponJpaRepository extends JpaRepository<Coupon, Long> {
+
+    Coupon save(Coupon coupon);
+
+    Optional<Coupon> findById(Long id);
+
+    void deleteById(Long id);
+
+    List<Coupon> findAllByIdIn(List<Long> ids);
+
+    int countAllByIdIn(List<Long> ids);
+}
---
+++ backend/market-api/src/main/java/com/sleekydz86/server/market/voucher/infrastructure/CouponRepositoryImpl.java
@@ -0,0 +1,48 @@
+package com.sleekydz86.server.market.voucher.infrastructure;
+
+import com.sleekydz86.server.market.coupon.domain.Coupon;
+import com.sleekydz86.server.market.coupon.domain.CouponRepository;
+import com.sleekydz86.server.market.voucher.infrastructure.mapper.CouponMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Repository
+public class CouponRepositoryImpl implements CouponRepository {
+
+    private final CouponMapper couponMapper;
+
+    @Override
+    public Coupon save(final Coupon coupon) {
+        couponMapper.save(coupon);
+        return coupon;
+    }
+
+    @Override
+    public Optional<Coupon> findById(final Long id) {
+        return couponMapper.findById(id);
+    }
+
+    @Override
+    public void deleteById(final Long id) {
+        couponMapper.deleteById(id);
+    }
+
+    @Override
+    public List<Coupon> findAllByIdsIn(final List<Long> couponIds) {
+        return couponMapper.findAllByIdIn(couponIds);
+    }
+
+    @Override
+    public int countAllByIdIn(final List<Long> couponIds) {
+        return couponMapper.countAllByIdIn(couponIds);
+    }
+
+    @Override
+    public boolean isExistedById(final Long couponId) {
+        return couponMapper.findById(couponId).isPresent();
+    }
+}
